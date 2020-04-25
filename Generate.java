3977650a636690b8a511ab9b4aa97355f0cca618
Dhruv Batra <dhruvbatra2@gmail.com>
2020-04-25 14:06:58 -0400

    linkedList

--- Generate.java
+++ Generate.java
@@ -7,7 +7,7 @@ public class Generate
    //in main method make sure to throws FileNotFoundException
    public static void main(String[] args) throws FileNotFoundException
 	{
-      generate("Computer Science is very cool"," ","Arraylist","String","data","default","default");
+      generate("Computer Science is very cool"," ","linkedlist","String","data","default","default");
    }
    
    public static void generate(String str, String delim, String datType, String varType, String name, String caseFirst, String caseRest) throws FileNotFoundException
@@ -29,12 +29,12 @@ public class Generate
       switch(datType.toLowerCase())
       {
          case "array": genArray(str,delim,datType, varType,name,caseFirst,caseRest,question); break;
-         case "arraylist": genArrayList(str,delim,datType, varType,name,caseFirst,caseRest,question);; break;
-         //case "linkedlist": output = genLinkedList(name,text, varType,question,answer); break;
-         //case "stack": output = genStack(name,text, varType,question,answer); break;
-         //case "queue": output = genQueue(name,text, varType,question,answer); break;
-         //case "hashset": output = genHashSet(name,text, varType,question,answer); break;
-         //case "treeset": output = genTreeSet(name,text, varType,question,answer); break;
+         case "arraylist": genArrayList(str,delim,datType, varType,name,caseFirst,caseRest,question); break;
+         case "linkedlist": genLinkedList(str,delim,datType, varType,name,caseFirst,caseRest,question); break;
+         //case "stack": output = genStack(str,delim,datType, varType,name,caseFirst,caseRest,question); break;
+         //case "queue": output = genQueue(str,delim,datType, varType,name,caseFirst,caseRest,question); break;
+         //case "hashset": output = genHashSet(str,delim,datType, varType,name,caseFirst,caseRest,question); break;
+         //case "treeset": output = genTreeSet(str,delim,datType, varType,name,caseFirst,caseRest,question); break;
       }
    }
    
@@ -58,6 +58,7 @@ public class Generate
       question.println("import java.util.*;\npublic class arrayLab\n{\n\tpublic static void main(String[] args)\n\t{\n\t\t//Code Here\n\n\t}\n}");
       question.close();
    }
+   
    private static void genArrayList(String str, String delim, String datType, String varType, String name, String caseFirst, String caseRest, PrintWriter question)
    {
       String[] text = str.split(delim);
@@ -96,6 +97,51 @@ public class Generate
       question.println("\t8. Print the index of the last occurence of ["+text[num4]+"]");
       question.println("\t9. Remove element "+num6);
       question.println("\t10. Print each element of the ArrayList on a new line\n*/\n");
+      question.println("import java.util.*;\npublic class arrayLab\n{\n\tpublic static void main(String[] args)\n\t{\n\t\t//Code Here\n\n\t}\n}");
+      question.close();
+   }
+   
+   private static void genLinkedList(String str, String delim, String datType, String varType, String name, String caseFirst, String caseRest, PrintWriter question)
+   {
+      String[] text = str.split(delim);
+      int num5=(int)(Math.random() * text.length);
+      int num7=(int)(Math.random() * text.length);
+      int num8=(int)(Math.random() * text.length);
+      int num11=(int)(Math.random() * text.length);
+   
+      question.println("//LinkedList Lab Questions - DATA STRUCTURE GENERATOR\n");
+      question.println("/*Objectives:");
+      question.println("\t1. Create a "+varType+ " LinkedList with the following values: "+Arrays.toString(text));
+      question.println("\t2. Add ["+text[num5]+"] to the end of the LinkedList");
+      question.println("\t3. Add ["+text[num11]+"] to the beginning of the LinkedList");
+      question.println("\t4. Add ["+text[num7]+"] to the LinkedList at index "+num8);
+      
+      int num1=-1;
+      int num2=-1;
+      while(num1==num2)
+      {
+         num1=(int)(Math.random() * text.length); 
+         num2=(int)(Math.random() * text.length);
+      }
+      int num3=-1;
+      int num4=-1;
+      while(num3==num4)
+      {
+         num3=(int)(Math.random() * text.length); 
+         num4=(int)(Math.random() * text.length);
+      }
+      int num6=(int)(Math.random() * text.length);
+      int num9=(int)(Math.random() * text.length);
+      int num10=(int)(Math.random() * text.length);
+
+      question.println("\t5. Print the size of the LinkedList");
+      question.println("\t6. Switch element "+num1+" and element "+num2);
+      question.println("\t7. Replace the value of element "+num3+" with ["+text[num4]+"]");
+      question.println("\t8. Print the index of the first occurence of ["+text[num9]+"]");
+      question.println("\t10. Remove element "+num6);
+      question.println("\t11. Remove and print the first item in the LinkedList");
+      question.println("\t12. Remove and print the last item in the LinkedList");
+      question.println("\t13. Print each element of the LinkedList on a new line\n*/\n");
       question.println("import java.util.*;\npublic class arrayLab\n{\n\tpublic static void main(String[] args)\n\t{\n\t\t//Code Here\n\n\t}\n}");
       question.close();
    }
